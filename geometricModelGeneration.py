e87bbde2c6a5aa455f2e49d8f7455c76a60d3f08
Tzuria <64027915+TzuriR@users.noreply.github.com>
2020-11-01 20:20:29 +0200

    Add files via upload

---
+++ geometricModelGeneration.py
@@ -0,0 +1,23 @@
+
+import networkx as nx
+import matplotlib.pyplot as plt
+# Generate geometric graph
+# check on a large n!
+# how to make on R^d when d>2?
+# draw [0,1]^2
+
+
+def genGraph(n, r):
+    # n = 30
+    # radius = 0.4
+    # dim = 2
+    # p = 2
+    # random_geometric_graph(n, radius, dim=2, pos=None, p=2, seed=None)
+    # pos = {i: (random.gauss(0, 1), random.gauss(0, 1)) for i in range(n)}
+    # G = nx.random_geometric_graph(n=n, radius=radius, dim=dim, pos=pos, p=p, seed=None)
+
+    G = nx.random_geometric_graph(n=n, radius=r)
+    nx.draw(G, with_labels=True)
+    plt.savefig("simple_path.png")
+    plt.show()
+    return G
