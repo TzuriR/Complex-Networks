bb8ef21a0faa30987c1505c4d14f8800ab45606a
Tzuria <64027915+TzuriR@users.noreply.github.com>
2020-11-02 10:45:48 +0200

    Update posaAlgorithm.py

--- posaAlgorithm.py
+++ posaAlgorithm.py
@@ -1,16 +1,16 @@
-#Posa algorithm
-def Posa(g):
+# Posa algorithm
+def posa(g):
     # Step 1-------------------------------------------------------
 
     print("Start of step 1:")
     n = len(g.nodes)
-    railV = []
-    railE = []
-    #We are starting from the first node - insert it to the rail and call to utility function
+    rail_v = []
+    rail_e = []
+    # We are starting from the first node - insert it to the rail and call to utility function
     x = 0
-    railV.append(x)
-    PosaLoop(g, x, railV, railE)
-    return railV, railE
+    rail_v.append(x)
+    posa_loop(g, x, rail_v, rail_e)
+    return rail_v, rail_e
 
 
 # -----------------------------------------------------------------
@@ -19,191 +19,197 @@ def Posa(g):
 # --------------------------------------------------------------------------------
 # Utility function, get graph and node, and do the 3 steps of building an hamiltonian cycle
 
-def PosaLoop(g, x, railV, railE):
+def posa_loop(g, x, rail_v, rail_e):
     # call utility function that create initial path
-    insertToRail(g, x, railV, railE)
-    print("railV:")
-    print(railV)
-    print("railE:")
-    print(railE)
+    insert_to_rail(g, x, rail_v, rail_e)
+    print("rail_v:")
+    print(rail_v)
+    print("rail_e:")
+    print(rail_e)
 
     # Step 2-------------------------------------------------------
 
     print("Start of step 2:")
     # Check if the path is not hamiltonian
-    if len(railV) < len(g.nodes):
-        if len(railV) < 3:
+    if len(rail_v) < len(g.nodes):
+        if len(rail_v) < 3:
             exit('algorithm failed to find a path!')  # algorithm failed
-        # If the algorithm didnt fail - call utility function that make it hamiltonian
-        railV, railE = rotExt(g, railV, railE)
-    print("railV:")
-    print(railV)
-    print("railE:")
-    print(railE)
+        # If the algorithm didn't fail - call utility function that make it hamiltonian
+        rail_v, rail_e = rot_ext(g, rail_v, rail_e)
+    print("rail_v:")
+    print(rail_v)
+    print("rail_e:")
+    print(rail_e)
 
     # Step 3-------------------------------------------------------
     print("Start of step 3:")
     # Check if the path is hamiltonian
-    if len(railV) == len(g.nodes):
+    if len(rail_v) == len(g.nodes):
         # Call utility function that will make it cycle
-        railV, railE = makeCycle(g, railV, railE)
-        print("railV:")
-        print(railV)
-        print("railE:")
-        print(railE)
+        rail_v, rail_e = make_cycle(g, rail_v, rail_e)
+        print("rail_v:")
+        print(rail_v)
+        print("rail_e:")
+        print(rail_e)
     # Check if the path is not hamiltonian - go back
     else:
-        PosaLoop(g, railV[len(railV)-1], railV, railE)
+        posa_loop(g, rail_v[len(rail_v) - 1], rail_v, rail_e)
 
-    return railV, railE
+    return rail_v, rail_e
+
 
 # --------------------------------------------------------------------------------
-# Utility function- get hamitonian path and convert it to hamiltonian cycle
+# Utility function- get hamiltonian path and convert it to hamiltonian cycle
 
-def makeCycle(g, railV, railE):
-    #If there is (x0, xn) - add it to the rail and finish
-    if (isAt(g.edges, (railV[0], railV[len(railV) - 1])) == 1) or (
-            isAt(g.edges, (railV[len(railV) - 1], railV[0])) == 1):
-        railE.append((railV[len(railV) - 1], railV[0]))
-        return railV, railE
+def make_cycle(g, rail_v, rail_e):
+    # If there is (x0, xn) - add it to the rail and finish
+    if (is_at(g.edges, (rail_v[0], rail_v[len(rail_v) - 1])) == 1) or (
+            is_at(g.edges, (rail_v[len(rail_v) - 1], rail_v[0])) == 1):
+        rail_e.append((rail_v[len(rail_v) - 1], rail_v[0]))
+        return rail_v, rail_e
     i = 0
     flag = 0
-    #Run all over the nodes - if there are edge (xi+1,x1), (xi,xn) which are not belongs to the rail - call utility function swapCycle
-    for v in railV:
-        if i == len(railV) - 1:
+    # Run all over the nodes - if there are edge (xi+1,x1), (xi,xn) which are not belongs to the rail - call swapCycle
+    for v in rail_v:
+        if i == len(rail_v) - 1:
             break
-        x0 = railV[0]
+        x0 = rail_v[0]
         xi = v
-        xiplusone = railV[i + 1]
-        xn = railV[len(railV) - 1]
-        if ((isAt(g.edges, (x0, xiplusone)) == 1) or (isAt(g.edges, (xiplusone, x0)) == 1)) and (
-                (isAt(railV, (xiplusone, x0)) == 0) or (isAt(railV, (x0, xiplusone)) == 0)):
-            if ((isAt(g.edges, (xi, xn)) == 1) or (isAt(g.edges, (xn, xi)) == 1)) and (
-                    (isAt(railV, (xn, xi)) == 0) or (isAt(railV, (xi, xn)) == 0)):
-                if isAt(railE, (xi, xiplusone)) == 1:
-                    railV, railE = swapCycle(x0, xi, xiplusone, xn, railV, railE)
+        xi_plus_one = rail_v[i + 1]
+        xn = rail_v[len(rail_v) - 1]
+        if ((is_at(g.edges, (x0, xi_plus_one)) == 1) or (is_at(g.edges, (xi_plus_one, x0)) == 1)) and (
+                (is_at(rail_v, (xi_plus_one, x0)) == 0) or (is_at(rail_v, (x0, xi_plus_one)) == 0)):
+            if ((is_at(g.edges, (xi, xn)) == 1) or (is_at(g.edges, (xn, xi)) == 1)) and (
+                    (is_at(rail_v, (xn, xi)) == 0) or (is_at(rail_v, (xi, xn)) == 0)):
+                if is_at(rail_e, (xi, xi_plus_one)) == 1:
+                    rail_v, rail_e = swap_cycle(x0, xi, xi_plus_one, xn, rail_v, rail_e)
                     flag = 1  # we back from swap
                     break
         i += 1
     if flag == 0:
         exit('algorithm failed to find a cycle!')  # algorithm failed
-    return railV, railE
+    return rail_v, rail_e
+
 
 # --------------------------------------------------------------------------------
-# Utility function: do the swap of the rotation extenstion
+# Utility function: do the swap of the rotation extension
 # This function add the edges (xi+1,x1), (xi,xn) and delete the edge (xi, xi+1)
 
-def swapCycle(x0, xi, xiplusone, xn, railV, railE):
-    tempV = []
-    tempE = []
+
+def swap_cycle(x0, xi, xi_plus_one, xn, rail_v, rail_e):
+    temp_v = []
+    temp_e = []
     index = 0
-    while railV[index] != xiplusone:
-        tempV.append(railV[index])
+    while rail_v[index] != xi_plus_one:
+        temp_v.append(rail_v[index])
         if index != 0:
-            tempE.append((railV[index - 1], railV[index]))
+            temp_e.append((rail_v[index - 1], rail_v[index]))
         index += 1
-    tempV.append(xn)
-    tempE.append((xi, xn))
-    # endIndex run on railV, index run on tempV
+    temp_v.append(xn)
+    temp_e.append((xi, xn))
+    # endIndex run on rail_v, index run on tempV
     index += 1
-    endIndex = len(railV) - 2
-    while endIndex >= railV.index(xiplusone):
-        tempV.append(railV[endIndex])
-        tempE.append((railV[endIndex + 1], railV[endIndex]))  # How direct from railE?
-        endIndex -= 1
+    end_index = len(rail_v) - 2
+    while end_index >= rail_v.index(xi_plus_one):
+        temp_v.append(rail_v[end_index])
+        temp_e.append((rail_v[end_index + 1], rail_v[end_index]))  # How direct from rail_e?
+        end_index -= 1
         index += 1
-    tempE.append((xiplusone, x0))
-    railV = tempV
-    railE = tempE
-    return railV, railE
+    temp_e.append((xi_plus_one, x0))
+    rail_v = temp_v
+    rail_e = temp_e
+    return rail_v, rail_e
+
 
 # --------------------------------------------------------------------------------
-# Utility function, get rail and make it hamitonian
+# Utility function, get rail and make it hamiltonian
 
-def rotExt(g, railV, railE):
-    xt = railV[len(railV) - 1]
+def rot_ext(g, rail_v, rail_e):
+    xt = rail_v[len(rail_v) - 1]
     # Run all over x's neighbors and insert them into arrays
-    adjX = []
+    adj_x = []
     for e in g.edges:
         if e[0] == xt:
-            adjX.append(e[1])
+            adj_x.append(e[1])
         if e[1] == xt:
-            adjX.append(e[0])
+            adj_x.append(e[0])
 
-    # Do the extention rotation
-    if len(adjX) <= 1:
+    # Do the extension rotation
+    if len(adj_x) <= 1:
         exit('algorithm failed to find a path!')  # algorithm failed
     flag = 0  # tell us if we did swap
-    for xi in adjX:
-        if xi == railV[len(railV) - 2]:
+    for xi in adj_x:
+        if xi == rail_v[len(rail_v) - 2]:
             continue
-        if isAt(railE, (xi, xt)) == 1:
+        if is_at(rail_e, (xi, xt)) == 1:
             continue
-        xiplusone = railV[railV.index(xi) + 1]
-        adjXiplusOne = []
+        xi_plus_one = rail_v[rail_v.index(xi) + 1]
+        adj_xi_plus_one = []
         for e in g.edges:
-            if e[0] == xiplusone:
-                adjXiplusOne.append(e[1])
-            if e[1] == xiplusone:
-                adjXiplusOne.append(e[0])
-        for xk in adjXiplusOne:
-            # if isAt(railE, (xiplusone, xk)) == 1 or isAt(railE, (xk, xiplusone)) == 1:
-            if isAt(railV, xk) == 1:
+            if e[0] == xi_plus_one:
+                adj_xi_plus_one.append(e[1])
+            if e[1] == xi_plus_one:
+                adj_xi_plus_one.append(e[0])
+        for xk in adj_xi_plus_one:
+            # if isAt(rail_e, (xi_plus_one, xk)) == 1 or isAt(rail_e, (xk, xi_plus_one)) == 1:
+            if is_at(rail_v, xk) == 1:
                 continue
             # Call utility function that to the swap - add the edge (xi+1, xk), (xi,xt) and delete the edge (xi+1, xk)
-            railV, railE = swapRail(xi, xiplusone, xk, xt, railV, railE)
+            rail_v, rail_e = swap_rail(xi, xi_plus_one, xk, xt, rail_v, rail_e)
             flag = 1  # we back from swap
             break
         break
     if flag == 0:
         exit('algorithm failed to find a path!')  # algorithm failed
-    return railV, railE
+    return rail_v, rail_e
+
 
 # --------------------------------------------------------------------------------
-# Utility function: do the swap of the rotation extenstion
+# Utility function: do the swap of the rotation extension
 
-def swapRail(xi, xiplusone, xk, xt, railV, railE):
-    tempV = []
-    tempE = []
+def swap_rail(xi, xi_plus_one, xk, xt, rail_v, rail_e):
+    temp_v = []
+    temp_e = []
     index = 0
-    while railV[index] != xiplusone:
-        tempV.append(railV[index])
+    while rail_v[index] != xi_plus_one:
+        temp_v.append(rail_v[index])
         if index != 0:
-            tempE.append((railV[index - 1], railV[index]))
+            temp_e.append((rail_v[index - 1], rail_v[index]))
         index += 1
-    tempV.append(xt)
-    tempE.append((xi, xt))
-    # endIndex run on railV, index run on tempV
+    temp_v.append(xt)
+    temp_e.append((xi, xt))
+    # endIndex run on rail_v, index run on tempV
     index += 1
-    endIndex = len(railV) - 2
-    while endIndex >= railV.index(xiplusone):
-        tempV.append(railV[endIndex])
-        tempE.append((railV[endIndex + 1], railV[endIndex]))  # How direct from railE?
-        endIndex -= 1
+    end_index = len(rail_v) - 2
+    while end_index >= rail_v.index(xi_plus_one):
+        temp_v.append(rail_v[end_index])
+        temp_e.append((rail_v[end_index + 1], rail_v[end_index]))  # How direct from rail_e?
+        end_index -= 1
         index += 1
-    tempV.append(xk)
-    tempE.append((xiplusone, xk))
-    railV = tempV
-    railE = tempE
-    return railV, railE
+    temp_v.append(xk)
+    temp_e.append((xi_plus_one, xk))
+    rail_v = temp_v
+    rail_e = temp_e
+    return rail_v, rail_e
+
 
 # --------------------------------------------------------------------------------
 
 # Utility function: get node and rails, and insert the neighbors of the node into the rail
-def insertToRail(g, x, railV, railE):
+def insert_to_rail(g, x, rail_v, rail_e):
     # Run all over x's neighbors and insert them into array
-    adjX = []
+    adj_x = []
     for e in g.edges:
         if e[0] == x:
-            adjX.append(e[1])
+            adj_x.append(e[1])
         if e[1] == x:
-            adjX.append(e[0])
+            adj_x.append(e[0])
     # Run all over x's neighbors, choose one that is not in the rail, insert it and call recursion
-    for y in adjX:
-        if isAt(railV, y) == 0:
-            railV.append(y)
-            railE.append((x, y))
-            insertToRail(g, y, railV, railE)
+    for y in adj_x:
+        if is_at(rail_v, y) == 0:
+            rail_v.append(y)
+            rail_e.append((x, y))
+            insert_to_rail(g, y, rail_v, rail_e)
             break
     '''
     # Run all over x's neighbors and insert them into arrays
@@ -217,35 +223,36 @@ def insertToRail(g, x, railV, railE):
     print("x:")
     print(x)
     flag = 0
-    # Choose one of x's neighbors right that not exists at railV
+    # Choose one of x's neighbors right that not exists at rail_v
     for y in adjXR:
         print("y:")
         print(y)
-        if isAt(railV, y) == 1:
+        if isAt(rail_v, y) == 1:
             continue
         else:
-            railV.append(y)
-            railE.append((x, y))
-            insertToRail(g, y, railV, railE)
+            rail_v.append(y)
+            rail_e.append((x, y))
+            insertToRail(g, y, rail_v, rail_e)
             flag = 1
             break
 
     if flag != 1:
-        # Choose one of x's neighbors left that not exists at railV
+        # Choose one of x's neighbors left that not exists at rail_v
         for z in adjXL:
             print("z:")
             print(z)
-            if isAt(railV, z) == 1:
+            if isAt(rail_v, z) == 1:
                 continue
             else:
-                railV.append(z)
-                railE.append((x, z))
-                insertToRail(g, z, railV, railE)
+                rail_v.append(z)
+                rail_e.append((x, z))
+                insertToRail(g, z, rail_v, rail_e)
                 break
     '''
 
+
 # Utility function: get element and arr, return 1 if the element is at the array and 0 if not
-def isAt(arr, ele):
+def is_at(arr, ele):
     for a in arr:
         if a == ele:
             return 1
