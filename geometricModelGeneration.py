4f962ce1a9fbf30475d2f9e7d40c9fcba2d8caf5
Tzuria <64027915+TzuriR@users.noreply.github.com>
2020-11-09 14:43:41 +0200

    Update geometricModelGeneration.py

--- geometricModelGeneration.py
+++ geometricModelGeneration.py
@@ -1,22 +1,27 @@
-
 import networkx as nx
 import matplotlib.pyplot as plt
+import random as rd
 # Generate geometric graph
-# check on a large n!
 # how to make on R^d when d>2?
 # draw [0,1]^2
 
 
+# Generate graph
 def gen_graph(n, r):
-    # n = 30
-    # radius = 0.4
-    # dim = 2
-    # p = 2
-    # random_geometric_graph(n, radius, dim=2, pos=None, p=2, seed=None)
-    # pos = {i: (random.gauss(0, 1), random.gauss(0, 1)) for i in range(n)}
-    # G = nx.random_geometric_graph(n=n, radius=radius, dim=dim, pos=pos, p=p, seed=None)
-
-    g = nx.random_geometric_graph(n=n, radius=r)
+    # n = 100
+    # r = 0.2
+    dim = 2
+    p = 2
+    # g = nx.random_geometric_graph(n=n, radius=r)
+    pos = {i: (rd.uniform(0, 1), rd.uniform(0, 1)) for i in range(n)}
+    g = nx.random_geometric_graph(n=n, radius=r, dim=dim, pos=pos, p=p, seed=None)
+    # The distance between a and b
+    '''i = 0
+    for v in g.nodes:
+        print("v:", v)
+        x, y = g.nodes[i]['pos']
+        print("x, y:", x, y)
+        i += 1'''
     nx.draw(g, with_labels=True)
     plt.savefig("simple_path.png")
     plt.show()
