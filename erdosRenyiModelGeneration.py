e500a1e3e01495745833f10e8d7401002a57c061
idonissimian <63245643+idonissimian@users.noreply.github.com>
2020-11-09 14:51:29 +0200

    Update erdosRenyiModelGeneration.py

--- erdosRenyiModelGeneration.py
+++ erdosRenyiModelGeneration.py
@@ -1,16 +1,12 @@
-from networkx.generators.random_graphs import erdos_renyi_graph
 import networkx as nx
 import matplotlib.pyplot as plt
 
 
 # Generate graph
 def gen_graph(n, p):
-    # Random------------------------
-    g = erdos_renyi_graph(n, p)
-    print("g.nodes:")
-    print(g.nodes)
-    print("g.edges:")
-    print(g.edges)
+    g = nx.generators.random_graphs.erdos_renyi_graph(n, p)
+    print("g.nodes:", g.nodes)
+    print("g.edges:", g.edges)
     nx.draw(g, with_labels=True)
     plt.savefig("simple_path.png")
     plt.show()
@@ -21,7 +17,7 @@ def gen_graph(n, p):
     g.edges:
     [(0, 1), (0, 2), (0, 5), (0, 7), (0, 8), (1, 3), (1, 5), (1, 6), (1, 8), (1, 9), (2, 3), (2, 4), (2, 5), (2, 6), 
     (2, 8), (3, 5), (3, 7), (3, 8), (3, 9), (4, 5), (4, 7), (5, 6), (5, 7), (5, 8), (6, 7), (6, 9), (7, 8)]
-    
+
     # Example: do cycle with rotation - 10
     g = nx.Graph()
     g.add_nodes_from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
@@ -49,7 +45,7 @@ def gen_graph(n, p):
     nx.draw(G, with_labels=True)
     plt.savefig("simple_path.png")
     plt.show()
-    
+
     #Example: do cycle without rotation - 6
     g = nx.Graph()
     g.add_nodes_from([0, 1, 2, 3, 4, 5])
@@ -64,7 +60,7 @@ def gen_graph(n, p):
     nx.draw(G, with_labels=True)
     plt.savefig("simple_path.png")
     plt.show()
-    
+
     #Example: do cycle without rotation - 10
     g = nx.Graph()
     g.add_nodes_from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
