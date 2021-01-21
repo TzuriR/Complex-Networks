83009a2fdf1bf5242580c27824525c1da886ba05
idonissimian <63245643+idonissimian@users.noreply.github.com>
2021-01-21 21:51:41 +0200

    Add files via upload

---
+++ run_posa_gnp.py
@@ -0,0 +1,18 @@
+import posaAlgorithm
+import erdosRenyiModelGeneration
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Call function
+n = 40
+p = 0.5
+g = erdosRenyiModelGeneration.gen_graph(n, p)
+rail_v_gnp, rail_e_gnp = posaAlgorithm.posa(g)
+
+pos = nx.spring_layout(g)
+nx.draw_networkx_nodes(g, pos, cmap=plt.get_cmap('jet'), node_size=500)
+nx.draw_networkx_labels(g, pos)
+nx.draw_networkx_edges(g, pos, edgelist=g.edges, edge_color='k', arrows=True)
+nx.draw_networkx_edges(g, pos, edgelist=rail_e_gnp, edge_color='r', arrows=True)
+plt.savefig("posa_path_drawing.png")
+plt.show()
