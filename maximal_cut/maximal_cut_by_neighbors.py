e545b589f109a3a86c60e2d5a52881742c05c6fb
Tzuria <64027915+TzuriR@users.noreply.github.com>
2021-05-26 19:05:15 +0300

    Update maximal_cut_by_neighbors.py

--- maximal_cut/maximal_cut_by_neighbors.py
+++ maximal_cut/maximal_cut_by_neighbors.py
@@ -3,6 +3,7 @@ import geometric_network as ge_net
 import numpy as np
 import point_gnc
 import gnc_geometric_network
+# import maximal_cut_gnr
 
 
 # -----------------------------------------------------------------------------------------------------
@@ -45,7 +46,65 @@ def separate_to_sets_with_equal_probability(net, set_a, set_b):
     # net.print_point_arr(set_a)
     # print("set_b :")
     # net.print_point_arr(set_b)
-    return set_a, set_b
+    print("len of a :", len(set_a))
+    print("len of b :", len(set_b))
+    print("sum of a+b :", len(set_a) + len(set_b))
+    len_of_cut = cal_len_of_cut(net, set_a, set_b)
+    print("len of cut :", len_of_cut)
+    # For G(n,r) : need to divide len of edges in 2
+    print("len of edges :", len(net.edges) / 2)
+    print("len of cut / num of edges :", 2 * len_of_cut / len(net.edges))
+    '''# For G(n,c) : not need to divide len of edges in 2
+    print("len of edges :", len(net.edges))
+    print("len of cut / num of edges :", len_of_cut / len(net.edges))'''
+    return set_a, set_b, len_of_cut
+
+
+# -----------------------------------------------------------------------------------------------------
+# Bfs algorithm :
+
+def maximal_cut_bfs_algorithm(net, set_a, set_b):
+    # Initialize q and marked
+    q = []
+    marked = [0]*len(net.nodes)
+    # While there is unmarked node
+    while 0 in marked:
+        # Heuristic start node
+        # start_index = random.randint(0, len(net.nodes))
+        start_index = marked.index(0)
+        set_a.append(net.nodes[start_index])
+        marked[start_index] = 1
+        q.append(net.nodes[start_index])
+        # While q is not empty
+        while len(q) != 0:
+            cur = q.pop(0)
+            for temp_node in net.nodes:
+                if not net.is_at_edge_by_points(net.edges, cur.serial_number, temp_node.serial_number):
+                    continue
+                # Run all over cur neighbors - if it is not marked - insert to the opposite group of cur
+                if marked[temp_node.serial_number] == 0:
+                    q.append(temp_node)
+                    marked[temp_node.serial_number] = 1
+                    if cur in set_a:
+                        set_b.append(temp_node)
+                    if cur in set_b:
+                        set_a.append(temp_node)
+    # print("set_a :")
+    # net.print_point_arr(set_a)
+    # print("set_b :")
+    # net.print_point_arr(set_b)
+    print("len of a :", len(set_a))
+    print("len of b :", len(set_b))
+    print("sum of a+b :", len(set_a) + len(set_b))
+    len_of_cut = cal_len_of_cut(net, set_a, set_b)
+    print("len of cut :", len_of_cut)
+    '''# For G(n,r) : need to divide len of edges in 2
+    print("len of edges :", len(net.edges) / 2)
+    print("len of cut / num of edges :", 2 * len_of_cut / len(net.edges))'''
+    # For G(n,c) : not need to divide len of edges in 2
+    print("len of edges :", len(net.edges))
+    print("len of cut / num of edges :", len_of_cut / len(net.edges))
+    return set_a, set_b, len_of_cut
 
 
 # -----------------------------------------------------------------------------------------------------
@@ -107,33 +166,53 @@ def cal_len_of_cut(net, set_a, set_b):
 # -----------------------------------------------------------------------------------------------------
 
 def main():
-    # For G(n,r)
-    n = 10
-    r = 0.6
-    net = generate_model_gnr(n, r)
-    '''# For G(n,c)
-    n = 10
-    c = 0.1
-    net = generate_model_gnc(n, c)'''
+    '''# For G(n,r)
+    n = 100
+    r = 0.8
+    net = generate_model_gnr(n, r)'''
+    # For G(n,c)
+    n = 100
+    c = 0.5
+    net = generate_model_gnc(n, c)
     # net.print_edge_arr(net.edges)
     set_a = []
     set_b = []
-    set_a, set_b = separate_to_sets_with_equal_probability(net, set_a, set_b)
-    len_of_cut = cal_len_of_cut(net, set_a, set_b)
-    # print("len of cut after separate:", len_of_cut)
+
+    # ***********************************************************************************
+    # Heuristic starting points for algorithm 3
+
+    # Call algorithm 1
+    # print("********************************************")
+    # print("algorithm 1 :")
+    # set_a, set_b, len_of_cut = separate_to_sets_with_equal_probability(net, set_a, set_b)
+
+    # Call maximal_cut_gnr algorithm
+    # print("********************************************")
+    # print("maximal_cut_gnr algorithm :")
+    # set_a, set_b, len_of_cut = maximal_cut_gnr.maximal_cut_gnr_random_centers(net, set_a, set_b)
+
+    # Call maximal_cut_bfs algorithm
+    print("********************************************")
+    print("maximal_cut_bfs algorithm :")
+    set_a, set_b, len_of_cut = maximal_cut_bfs_algorithm(net, set_a, set_b)
+    # ***********************************************************************************
+
+    # Call algorithm 3
+    print("********************************************")
+    print("algorithm 3 :")
     set_a, set_b = update_sets_by_neighbors(net, set_a, set_b)
     len_of_cut = cal_len_of_cut(net, set_a, set_b)
-    print("len of cut :", len_of_cut)
     print("len of a :", len(set_a))
     print("len of b :", len(set_b))
     print("len of a+b :", len(set_a) + len(set_b))
-    # For G(n,r) : need to divide len of edges in 2
+    print("len of cut :", len_of_cut)
+    '''# For G(n,r) : need to divide len of edges in 2
     print("len of edges :", len(net.edges) / 2)
-    print("len of cut / num of edges :", 2 * len_of_cut / len(net.edges))
-    '''# For G(n,c) : not need to divide len of edges in 2
+    print("len of cut / num of edges :", 2 * len_of_cut / len(net.edges))'''
+    # For G(n,c) : not need to divide len of edges in 2
     print("len of edges :", len(net.edges))
-    print("len of cut / num of edges :", len_of_cut / len(net.edges))'''
-    net.draw_network("maximal_cut_by_neighbors", nodes_list=set_a)
+    print("len of cut / num of edges :", len_of_cut / len(net.edges))
+    # net.draw_network("maximal_cut_by_neighbors", nodes_list=set_a)
 
 
 if __name__ == '__main__':
