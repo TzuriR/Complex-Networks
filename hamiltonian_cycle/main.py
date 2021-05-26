fc01df018fea66cabb629df13af3e08d0a298e15
idonissimian <63245643+idonissimian@users.noreply.github.com>
2021-05-26 17:27:33 +0300

    Update main.py

--- hamiltonian_cycle/main.py
+++ hamiltonian_cycle/main.py
@@ -1,8 +1,8 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import posa_improvement_for_geometric_model
-# import posa_algorithm
-
+import posa_algorithm
+import geometric_model_generation
 '''import posaWithNetworkClass
 import erdosRenyiNetwork
 import Point as Pnt
@@ -27,21 +27,23 @@ er_net.draw_network()
 rail_v, rail_e = posaWithNetworkClass.posa(er_net)
 '''
 
-'''
+
 # Call function
-n = 50
-p = 0.8
-r = 0.6
+n = 20
+r = 0.9
 # g = erdosRenyiModelGeneration.gen_graph(n, p)
 # rail_v_gnp, rail_e_gnp = posaAlgorithm.posa(g)
-g = geometricModelGeneration.gen_graph(n, r)
+g = geometric_model_generation.gen_graph(n, r)
 print("Posa improvement :")
-rail_v_geo_i, rail_e_geo_i = posaImprovementForGeometricModel.posa(g)
-print("Posa original :")
-rail_v_geo, rail_e_geo = posaAlgorithm.posa(g)
-'''
+rail_v_geo, rail_e_geo = posa_improvement_for_geometric_model.posa(g)
+print("len of rail :", len(rail_v_geo))
+if len(rail_e_geo) == n:
+    print("Found hamiltonian cycle")
+else:
+    print("Didn't find hamiltonian cycle")
 
-# I-M-P-O-R-T-A-N-T : case that Posa regular failed and Posa improve succeeds
+
+'''# I-M-P-O-R-T-A-N-T : case that Posa regular failed and Posa improve succeeds
 g = nx.Graph()
 g.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
 g.add_edge(1, 2)
@@ -62,9 +64,7 @@ g.add_edge(4, 9)
 g.add_edge(5, 9)
 g.add_edge(5, 10)
 g.add_edge(6, 10)
-'''nx.draw(g, with_labels=True)
-plt.savefig("network_drawing.png")
-plt.show()'''
+
 # rail_v = [1,2,3,4,5,6]
 # rail_e = [(1,2),(2,3),(3,4),(4,5),(5,6)]
 # rail_v, rail_e = posaImprovementForGeometricModel.absorb_vertices(g, rail_v, rail_e)
@@ -77,7 +77,7 @@ nx.draw_networkx_labels(g, pos)
 nx.draw_networkx_edges(g, pos, edgelist=g.edges, edge_color='k', arrows=True)
 nx.draw_networkx_edges(g, pos, edgelist=rail_e, edge_color='r', arrows=True)
 plt.savefig("posa_path_improvement_drawing.png")
-plt.show()
+plt.show()'''
 
 '''G = nx.random_geometric_graph(6, 0.5)
 edge_x = []
